ef4a1962bf86533da671c0a90eeb799c5f75ec9a
aka-sargsyan <akasargsyan87@gmail.com>
2022-03-26 02:45:16 +0400

    solved exercise number 1 and 2

--- homeWork_7.py
+++ homeWork_7.py
@@ -1,218 +1,143 @@
-# import builtins
-#
-# def is_builtins(variable_str):
-#     variable_list = dir(builtins)
-#     if variable_str in variable_list:
-#         return True
-#     else:
-#         return False
-#
-# print(f'is variable nam builtin {is_builtins("sum")}')
-#
-# print(globals())
+""" Dunder methods and exceptions """
 
-""" OOP """
+""" Ուշադրությու՛ն։ Ցանկացած տեղ, եթե ցանկանում եք exception raise անել, ստեղծեք ձեր սեփական exception կլասերը և
+ աշխատեք դրանց հետ։ """
 
-# 1. Create a calculator class. It will have 2 numerical instance attributes. Then declare 4 methods called add, sub,
-# mult, div to respectively add, subtract, multiply and divide the attributes.
-# Ստեղծել կալկուլատորի կլաս։ Այն պետք է ունենա երկու instance attribute։ Ապա ստեղծել չորս մեթոդ՝ add, sub,
-# mult, div, որոնք համապատասխանաբար կգումարեն, կհանեն, կբազմապատկեն և կբաժանեն այդ ատրիբուտները։
+# 1. Create a Color class. This will hold 3 instance variables, red, green, blue (RGB - info on this here:
+# https://en.wikipedia.org/wiki/RGB_color_model).
+# We want to be able to get new colors if we add instances of the color class. I'm not sure how much sense it will make
+# to also subtract the colors, but lets do it for fun! In summary, we can add 2 or more colors and get a new color
+# object with added RGB values (don't forget about the boundaries!), subtract them to get a new color object with
+# subtracted values. When printed, we want to have a nice representation of our color (maybe even the color itself?).
+# One more fun thing!
+# Optional - As colors are quite often represented in hexadecimals, lets override the corresponding function
+# such that when hex() is called on our class, we get the hex color code for our color.
+# Ստեղծել Color կլաս։ Այն կունենա երեք instance ատրիբուտ՝ red, green, blue (ըստ RGB գունային մոդելի։ Վերջինիս մասին
+# ավելի մանրամասն տեսեք այստեղ https://en.wikipedia.org/wiki/RGB_color_model): Մենք պետք է կարողանանք գումարել երկու
+# Color տիպի օբյեկտ և ստանանք նոր Color, որի RGB արժեքները կլինեն օպերանդների համապատասխան արժեքների գումարները։
+# Սահմանեք նաև տարբերության օպերատորը, որը կանի հակառակ գործողությունը (ուշադիր եղեք սահմաններին։ RGB արժեքները
+# գտնվում են (0, 255) տիրույթում): Հավելյալ, մենք ուզում ենք, որ մեր օբյեկտները ունենան գեղեցիկ ներկայացում, երբ նրանց
+# սթրինգի փոխակերպենք։ Կարող եք անգամ հենց համապատասխան գույնը տպել կոնսոլում (google-ը ձեզ օգնական :) ):
+# Լրացուցիչ։ Գույները շատ հաճախ ներկայացվում են 16-հիմնային համակարգում (hexadecimal)։ Կարո՞ղ եք այնպես անել, որ
+# կիրառելով hex() ֆունկցիան մեր օբյեկտի վրա ստանանք համապատասխան գույնի 16-հիմնային ներկայացումը։
 
-# first version
-
-# class Calculator:
+# class Color:
 #
-#     def __init__(self, number_1, number_2):
-#         self.add = number_1 + number_2
-#         self.sub = number_1 - number_2
-#         self.mult = number_1 * number_2
-#         self.div = number_1 / number_2
+#     def __init__(self, red, green, blue, text='hello'):
+#         self.blue = blue
+#         self.green = green
+#         self.red = red
+#         self.text = text
 #
+#     def __add__(self, other):
+#         if self.red + other.red > 255 or self.green + other.green > 255 or self.blue + other.blue > 255:
+#             raise TypeError('please enter less than 255')
+#         return Color(self.red + other.red, self.green + other.green, self.blue + other.blue, self.text + ' ' + other.text)
 #
-# calc = Calculator(7, 6)
-#
-# print(f'calculate numbers with add {calc.add}')
-# print(f'calculate numbers with sub {calc.sub}')
-# print(f'calculate numbers with mult {calc.mult}')
-# print(f'calculate numbers with div {calc.div}')
-
-# second version
-
-# class Calculator:
-#
-#     def __init__(self, *numbers):
-#
-#         current_list = numbers
-#         current_add = current_list[0]
-#         current_sub = current_list[0]
-#         current_mult = current_list[0]
-#         current_div = current_list[0]
-#
-#         for i in range(1, len(current_list)):
-#             current_add += current_list[i]
-#             current_sub -= current_list[i]
-#             current_mult *= current_list[i]
-#             current_div /= current_list[i]
-#
-#         self.add = current_add
-#         self.sub = current_sub
-#         self.mult = current_mult
-#         self.div = current_div
+#     def __sub__(self, other):
+#         if self.red - other.red < 0 or self.green - other.green < 0 or self.blue - other.blue < 0:
+#             raise TypeError('please enter positive number')
+#         return Color(self.red - other.red, self.green - other.green, self.blue - other.blue, self.text + ' ' + other.text)
 #
 #
-# calc = Calculator(7, 8, 9)
+# color_1 = Color(125, 70, 60, 'Artak')
+# print(f'\033[38;2;{color_1.red};{color_1.green};{color_1.blue}m{color_1.text}')
 #
-# print(f'calculate numbers with add {calc.add}')
-# print(f'calculate numbers with sub {calc.sub}')
-# print(f'calculate numbers with mult {calc.mult}')
-# print(f'calculate numbers with div {calc.div}')
-
-# 3-th version
-
-# class Calculator:
+# color_2 = Color(120, 60, 45, 'Sargsyan')
+# print(f'\033[38;2;{color_2.red};{color_2.green};{color_2.blue}m{color_2.text}')
 #
-#     add = 0
-#     sub = 0
-#     mult = 1
-#     div = 1
+# new_color_sum = color_1 + color_2
+# print(f'\033[38;2;{new_color_sum.red};{new_color_sum.green};{new_color_sum.blue}m{new_color_sum.text}')
 #
-#     def __init__(self, *numbers):
-#         self.array = numbers
-#
-#     def add_attribute(self):
-#         for item in self.array:
-#             self.add += item
-#
-#     def sub_attribute(self):
-#         current_list = self.array
-#         self.sub = current_list[0]
-#         for i in range(1, len(current_list)):
-#             self.sub -= current_list[i]
-#
-#     def mult_attribute(self):
-#         for item in self.array:
-#             self.mult *= item
-#
-#     def div_attribute(self):
-#         current_list = self.array
-#         self.div = current_list[0]
-#         for i in range(1, len(current_list)):
-#             if current_list[i] == 0:
-#                 raise ValueError('you can not divide by 0 ')
-#             else:
-#                 self.div /= current_list[i]
-#
-#
-#
-# calc = Calculator(7, 8, 5)
-# calc.add_attribute()
-# calc.sub_attribute()
-# calc.mult_attribute()
-# calc.div_attribute()
-#
-# print(f'calculate numbers with add {calc.add}')
-# print(f'calculate numbers with sub {calc.sub}')
-# print(f'calculate numbers with mult {calc.mult}')
-# print(f'calculate numbers with div {calc.div}')
-
-
-# 2. Create Animal class with three methods - make_noise(), walk() and fly(). The methods shall print what the animal is
-# doing. Then, create a Bird and Feline classes which inherit from Animal. Override the superclass methods, so that
-# a cat warns us that it can't fly and the bird only wants to fly as it is too lazy to walk.
-# Ստեղծել Animal կլասս, որը կունենա երեք մեթոդ՝ make_noise(), walk() և fly()։ Այդ մեթոդները պետք է տպեն, թե ինչ է անում
-# կենդանին։ Ապա ստեղծել Bird և Feline կլասեր, որոնք ժառանգում են Animal-ից։ Animal-ի մեթոդները իմպլեմենտացրեք
-# այդ կլասերի մեջ, այնպես, որ եթե կատուն փորձի թռչել, կոնսոլում տպվի, որ նա չի կարող թռչել։ Իսկ եթե թռչնի վրա
-# կիրառենք walk() մեթոդը, նա ասի, որ ալարում է և միայն կարող է թռչել։
-#
-# class Animal:
-#     def make_noise(self, noise):
-#         print(noise)
-#
-#     def walk(self, walk):
-#         print('walking')
-#
-#     def fly(self):
-#         print('flying')
-#
-# class Bird(Animal):
-#     def walk(self):
-#         print('only wants to fly as it is too lazy to walk')
-#
-# class Feline(Animal):
-#     def fly(self):
-#         print('cat do not fly')
-#
-# chicken = Bird()
-# chicken.walk()
-# chicken.make_noise('muuu')
-#
-# cat = Feline()
-# cat.fly()
+# new_color_sub = color_1 - color_2
+# print(f'\033[38;2;{new_color_sub.red};{new_color_sub.green};{new_color_sub.blue}m{new_color_sub.text}')
 
 
 
-# 3. Create a class called Shape. Define methods to calculate the shape's perimeter and the area.
-# Ստեղծել Shape անունով կլաս։ Սահմանել մեթոդներ, սակայն չիմպլեմենտացնել (մեթոդի մեջ պարզապես գրել pass) երկու մեթոդ,
-# դրանք անվանելով perimeter և area։
+# 2. Create a class named Length. The default unit for length is meter. The class must contain some conversions
+# information, e.g. feet -> m, km -> m, yard -> m, mile -> m etc.
+#    a) Create a dictionary that will hold the metrics. Keys will be the unit name and their values will be the
+#    coefficients for converting that unit to meters.
+#    b) The class will have 2 instance attributes. Units and the length value itself.
+#    c) Now, we can add lengths of course. So implement that method. But be careful, we can't add yard to meters, so
+#    you will need to convert everything before adding.
+#    d) Implement the __str__ method. This method must show the length of our Length object in meters.
+# Feel free to add some more features if you find them useful.
+# Ստեղծեք Length կլաս: Միավորի սկզբնական արժեքը մետրն է։ Կլասը պետք է պարունակի երկարության միավորների կոնվերտացիաների
+# հետ կապված որոշակի ինֆորմացիա։ Ավելի կոնկրետ հետևյալ միավորների՝ feet -> m, km -> m, yard -> m, mile -> m (ավելացրեք
+# այլ միավորներ, եթե ցանկանում եք)։
+#    a) Ստեղծեք dictionary, որը կպարունակի կոնվերտացիայի համար անհրաժեշտ հաստատունները։ key-երը պետք է լինեն
+#    միավորների անունները, իսկ արժեքները մետր դարձնելու համար գործակիցները։ {'km': 1000, ...}
+#    b) Կլասը պետք է ունենա երկու ատրիբուտ՝ երկարության արժեքը և միավորը։
+#    c) Երկարությունները կարող ենք գումարել։ Իմպլեմենտ արեք համապատասխան ֆունկցիան, որպեսզի length օբյեկտների վրա
+#    կարողանանք կիրառել + օպերատորը։
+#    d) Իմպլեմենտ արեք նաև __str__ մեթոդը։ Երբ մեր օբյեկտները սթրինգ դարձնենք, կուզենք տեսնել դրանց արժեքն ու միավորը։
 
-class Shape:
-    def __init__(self):
-        self.__dont_change = True
-    def __change_name(self):
-        self.__dont_change = False
+class Length:
+    name_type = {'km': 1000, 'yard' : 0.9144, 'mile': 1609.34}
+
+    def __init__(self, length, length_type):
+        self.length_type = length_type
+        self.length = length
+
+    def __add__(self, other):
+        for key, value in self.name_type.items():
+            if self.length_type == key:
+                self.length = value * self.length
+                break
+
+        for key, value in other.name_type.items():
+            if other.length_type == key:
+                other.length = value * other.length
+                self.other = key
+                break
+
+        return (self.length + other.length) / self.name_type[other.length_type]
 
     def __str__(self):
-        return 'self.__change_name()'
 
-kkk = Shape()
+        return self.length_type
 
-# kkk.__change_name(False)
-# print(kkk.__dont_change)
-print(kkk)
+    # def converter(self, new_self):
+    #     for key, value in new_self.name_type.items():
+    #         if new_self.length_type == key:
+    #             new_self.length = value * new_self.length
+    #             break
 
-# 4. Create a Circle class which will inherit from Shape. Implement the constructor and superclass methods to correctly
-# calculate the perimeter and the area of the circle.
-# Ստեղծել Circle կլաս, որը կժառանգի Shape-ից։ Իմպլեմենտ արեք կոնստրուկտորը (շրջանագծի դեպքում կունենանք մեկ instance
-# attribute` շառավիղը) և Shape-ի երկու մեթոդներն այնպես, որ Circle տիպի օբյեկտի վրա կիրառենք perimeter-ը, կստանանք
-# շրջանագծի երկարությունը, իսկ area-ն կիրառելու դեպքում՝ մակերեսը։
 
-# 5. Create a Rectangle class which will inherit from Shape. Implement the constructor and superclass methods to
-# correctly calculate the perimeter and the area of the circle.
-# Ստեղծել Rectangle կլաս, որը կժառանգի Shape-ից։ Իմպլեմենտ արեք կոնստրուկտորը և Shape-ի երկու մեթոդները ուղղանկյան համար
+km = Length(1, 'km')
 
-# 6. Create a Triangle class which will inherit from Shape. Implement the constructor and superclass methods to
-# correctly calculate the perimeter and the area of the circle. Note, that you must check whether a triangle with the
-# given lengths can exist.
-# Ստեղծել Triangle կլաս, որը կժառանգի Shape-ից։ Իմպլեմենտ արեք կոնստրուկտորը և Shape-ի երկու մեթոդները եռանկյան համար։
-# Եռանկյուն օբյեկտը ստեղծելուց անպայման ստուգել, թե արդյո՞ք տրված կողմերով եռանկյուն կարող է գոյություն ունենալ։
+yard = Length(1, 'yard')
 
-# 7. a) Create a Vehicle class. The class should have the following instance attributes: name, mileage (defaults to 0),
-#    condition, price, max_speed, current_speed and engine_on (defaults to False). It should also have
-#    unimplemented methods called start_engine, accelerate (takes a number as an argument), stop.
-#    b) Create 2 classes called ElectricVehicle and PetrolVehicle. These classes inherit from the Vehicle class.
-#    ElectricVehicle should have the following instance attributes: driving_range, charging_time. PetrolVehicle has the
-#    following instance attributes: engine (the volume in litres), transmission, num_of_gears, current_gear. Of course,
-#    these classes should call the parent class constructor in their constructor.
-#    c) Implement the methods from Vehicle class in each of the child classes accordingly. start_engine should notify us
-#    that the engine is on and change the engine_on attribute to True. accelerate method should accelerate the car
-#    with the amount of its acceleration attribute and accordingly change the velocity of the car. stop method reduces
-#    the speed to 0 and stops the engine. You should also do some checks, e.g. the speed can't be more than max_speed
-#    of the car, the car can't start with its engine off etc.
-#    d) For the PetrolCar, you should be able to change the transmission gears as well. If the transmission is manual,
-#    you should increase the current_gear as the speed increases (current_gear can't be more than the number_of_gears).
-#    e) Finally, create some petrol and electric vehicle objects and test your methods.
-#
-#    a) Ստեղծել Vehicle կլաս։ Կլասը պետք է ունենա հետևյալ instance attribute-ները - name, mileage (նախնական 0 է),
-#    condition, price, max_speed, current_speed and engine_on (նախնական False է)։ Կլասը պետք է ունենա նաև երեք
-#    չիմպլեմենտացված մեթոդ՝ start_engine, accelerate (ունի մեկ արգումենտ), stop.
-#    b) Ստեղծել երկու կլաս՝ ElectricVehicle և PetrolVehicle։ Այս կլասերը ժառանգում են Vehicle—ից։ Էլեկտրական մեքենան
-#    պետք է ունենա հետևյալ ատրիբուտները - driving_range, charging_time։ Իսկ վառելիքայինը՝ engine (ծավալը լիտրերով),
-#    transmission, num_of_gears, current_gear։ Այս կլասերը պետք է նաև կանչեն Vehicle-ի __init__-ը (super()-ի միջոցով):
-#    c) Իմպլեմենտ անել Vehicle-ից ժառանգված բոլոր մեթոդները։ start_engine-ը պետք է զգուշացնի, որ շարժիչը միացված է և
-#    engine_on ատրիբուտը դարձնի True. accelerate-ը պետք է բարձրացնի մեքենայի արագությունը, իսկ stop-ը պետք է
-#    արագությունն իջեցնի 0-ի և անջատի շարժիչը։ Պետք է նաև կատարել որոշակի վալիդացիա։ Օրինակ՝ արագութոյւնը չի կարող
-#    գերազանցել max_speed-ը, մեքենան չի կարող արագանալ, եթե դրա շարժիչը անջատված է և այլն։
-#    d) Վառելիքային մեքենայի համար պետք է նաև ներառել որոշակի տրամաբանություն փոխանցման տուփի հետ կապված։ Եթե այն
-#    մեխանիկական է, կախված արագությունից պետք է փոխել current_gear-ը (current_gear-ը չի կարող գերազանգել
-#    number_of_gears-ը):
-#    e) Վերջապես ստեղծել մի քանի էլեկտրական և վառելիքային մեքենա օբյեկտներ և թեստավորել բոլոր մեթոդները։
+
+print(str(km))
+
+
+# 3. Create an Atom class. We will have predefined atoms for it, which are C - carbon, N - nitrogen, H - hydrogen,
+# O - oxygen and P - phosphorus.
+#     a) Now, the class has an instance variable called "name".
+#     b) If you create an object with a name other than the 5 atoms mentioned, you should get an exception. But not any
+#     kind of exception. This exception will be something that we have defined. So, define an exception class called
+#     UnknownAtom. This must give us a detailed explanation why it is raised.
+#     c) Define a new class called Molecule. The molecule has an instance variable, which is a list.
+#     d) implement the __add__ method for our Atom class. Adding two atoms will return, you've guessed it, a Molecule!
+#     e) We can also add an atom to a molecule. So implement that functionality as well!
+#     f) Finally, do some cosmetic stuff, like implement the __str__ method for both of our classes.
+# If you're into chemistry, feel free to add some logic like keeping charges of each atom, tracking valency and raising
+# an error if two atoms can't make a molecule.
+# Ստեղծել Atom կլաս: Պետք է ունենանք նախապես հայտարարված ատոմների ցուցակ։ Դրանք են C - ածխածին, N - ազոտ, H - ջրածին,
+# O - թթվածին and P - ֆոսֆոր։
+#     a) Կլասը պետք է ունենա instance ատրիբուտ name։
+#     b) Եթե ստեղծեք ատոմ, որի անունը չկա նախապես հայտարարված ցուցակում, պետք է ստանաք exception: Այն չպետք է լինի
+#     built-in exception: Ստեղծեք ձեր exception-ը, որը կունենա UnknownAtom անունը և հնարավորինս պարզ կներկայացնի,
+#     թե ինչն է սխալ գնացել։
+#     c) Ստեղծել նոր կլաս, որի անունը կլինի Molecule: Այն պետք է ունենա մեկ instance ատրիբուտ, որը լինելու է որոշակի
+#     լիստ։
+#     d) Սահմանեք __add__ մեթոդը ատոմ կլասի համար։ Երկու ատոմ գումարելուց պետք է ստանանք, գուշակեցիք, մոլեկուլ։ Ստացված
+#     մոլեկուլը պետք է իր լիստի մեջ պարունակի այն ատոմների անունները ինչից ստացվել է։
+#     e) Մոլեկուլներին նույնպես հնարավոր է միացնել ատոմ։ Սահմանեք __add__ մեթոդը մոլեկուլների համար։ Ֆունկցիայի
+#     տրամաբանությունը ձեզ վրա է։
+#     f) Վերջապես սահմանեք __str__ մեթոդը երկու կլասերի համար և սահմանեք ատոմների ու մոլեկուլների համար գեղեցիկ
+#     սթրինգային ներկայացում։
+# Եթե քիմիայի սիրահար եք, կարող եք ավելացնել հավելյալ ֆունկցիոնալություն։ Օրինակ պահել ատոմների լիցքը, մոլեկուլ
+# ստեղծելուց հաշվի առնել վալենտականությունը և այլն։
